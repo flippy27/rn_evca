b217f298490ac89e565fd0c16447ede08a61ffa3
F.carrascoHantke <31319601+flippy27@users.noreply.github.com>
2023-08-24 10:13:02 -0400

    hola

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+const Stack = createNativeStackNavigator();
+
+export const WelcomeView = () => {
+  return <Text>home</Text>;
+};
+export const RegisterView = () => {
+  return <Text>register</Text>;
+};
+export const LoginView = () => {
+  return <Text>login</Text>;
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={WelcomeView} />
+        <Stack.Screen name="Profile" component={RegisterView} />
+        <Stack.Screen name="Settings" component={LoginView} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
